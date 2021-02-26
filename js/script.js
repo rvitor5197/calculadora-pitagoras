6af8150dc2e709ac6bc40b881a47bc5ec2324b38
Vitor Fonseca <rvitor5197@gmail.com>
2021-02-26 16:49:44 -0300

    add logica e fix layout

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function sum() {
+    var lado1, lado2, lado3, res;
+    lado1 = Number(document.getElementById('fnum').value);
+    lado2 = Number(document.getElementById('secondnum').value);
+    lado3 = Number(document.getElementById('thnum').value);
+    res = document.getElementById('res');
+
+    if (lado1 == "" || lado2 == "" || lado3 == "") {
+        alert('Insira os lados do triângulo');
+    }
+
+    if (lado1 == lado2 && lado3 == lado2) {
+        console.log('equilatero');
+        res.innerHTML = 'Equilátero';
+    } else if (lado1 == lado2 || lado3 == lado2 || lado3 == lado1) {
+        console.log('isoceles');
+        res.innerHTML = 'Isocéles';
+    } else {
+        console.log('Escaleno')
+        res.innerHTML = 'Escaleno';
+    }
+
+}
